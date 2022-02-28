2960ebf6367b6b6b324a459b08516caa58085e9a
Bhavna Jain <jbhavna732@gmail.com>
2022-02-28 16:00:15 +0530

    initial commit

---
+++ custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    ma5menu({
+        menu: '.site-menu',
+        activeClass: 'active',
+        footer: '#ma5menu-tools',
+        position: 'left',
+        closeOnBodyClick: true
+    });
+});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    mouseDrag:false,
+    autoplay:true,
+    animateOut: 'slideOutUp',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
